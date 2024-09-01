981e8c16f32610085918a339df60afe8f24491d9
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2024-09-01 14:54:13 +0200

    Add files via upload

---
+++ lesson16/script.js
@@ -0,0 +1,9 @@
+import data from 'https://www.jsonkeeper.com/b/QI82'
+assert {
+    type: "json"
+};
+
+$("#div1").load("data.txt");
+/*   $("#div1").click(function() {
+       $("#div1").load("data.txt");
+   });*/
